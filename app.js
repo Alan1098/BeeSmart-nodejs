6ea0c68523da2b43300cc4c9fc932ba166ff1bd1
Tu Nombre <tu@email.com>
2024-04-01 15:36:41 -0500

    parentesis

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
-
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+});
 
-app.get('/registro'), (req, res) => {
-    res.sen("bienvenido")
-}
-    
+app.get('/registro', (req, res) => {
+    res.send("¡Bienvenido!");
+});
